bf10cce14f62d25eba196fecd820bb563f38ed45
Austin Miles <j.austinmiles@outlook.com>
2019-08-28 10:38:02 -0400

    Add command line options for -a and -d (adding a task and finishing the day). Refactor code and add global variables

--- os_todo.py
+++ os_todo.py
@@ -7,6 +7,7 @@ TASK_NAME_COL = 0
 PRIORITY_COL = 1
 DAILY_COL = 2
 COMPLETED_COL = 3
+MAX_PRIORITY = 16
 
 
 def decide_task():
@@ -25,9 +26,9 @@ def get_rows(csv_file):
     for row in csvreader:
         if len(row) == 0:
             continue
-        elif row[3] == 'TRUE':
+        elif row[COMPLETED_COL] == 'TRUE' and row[DAILY_COL] != 'TRUE':
             continue
-        elif row[1] == '':
+        elif row[PRIORITY_COL] == '':
             continue
         rows.append(row)
         get_priority(priorities, row)
@@ -46,13 +47,15 @@ def get_priority(priorities, row):
 def mark_as_done():
     to_mark = decide_task()
     to_mark[COMPLETED_COL] = 'TRUE'
+    if to_mark[DAILY_COL] == 'TRUE':
+        to_mark[PRIORITY_COL] = str(1)
     rows = open_file_and_get_rows()
     replace_row(rows, to_mark)
     write_rows_to_file(rows)
 
 
 def write_rows_to_file(rows):
-    with open(FILE_NAME, 'w') as write_file:
+    with open(FILE_NAME, 'w', newline='') as write_file:
         c = csv.writer(write_file)
         c.writerows(rows)
         write_file.close()
@@ -71,9 +74,9 @@ def open_file_and_get_rows():
     return rows
 
 
-def mark_as_in_progress():
+def mark_as_in_progress(decrement):
     to_mark = decide_task()
-    num = int(to_mark[PRIORITY_COL]) - 1
+    num = int(to_mark[PRIORITY_COL]) - decrement
     to_mark[PRIORITY_COL] = str(num)
     rows = open_file_and_get_rows()
     replace_row(rows, to_mark)
@@ -83,16 +86,62 @@ def mark_as_in_progress():
 def main(argv):
     print(decide_task())
     try:
-        opts, args = getopt.getopt(argv, 'p')
+        opts, args = getopt.getopt(argv, 'pcxda')
     except getopt.GetoptError:
-        print("Use os_todo.py -p to postpone a task")
+        print("Use os_todo.py -p to postpone a task by 1 day")
+        print("Use os_todo.py -c to complete a task")
+        print("Use os_todo.py -x to extend the due date of a task by 5 days")
+        print("Use os_todo.py -d to increment the priorities of every undone task")
+        print("Use os_todo.py -a to add a new task")
         sys.exit(2)
     for opt, arg in opts:
         if opt == "-p":
-            mark_as_in_progress()
+            mark_as_in_progress(1)
+        elif opt == "-c":
+            mark_as_done()
+        elif opt == "-x":
+            mark_as_in_progress(5)
+        elif opt == "-d":
+            end_day()
+        elif opt == "-a":
+            add_task()
     print(decide_task())
+
+
+def end_day():
+    rows = open_file_and_get_rows()
+    for row in rows:
+        if row[PRIORITY_COL] == 'Priority':
+            continue
+        if row[COMPLETED_COL] == 'FALSE' and row[DAILY_COL] == 'TRUE':
+            row[PRIORITY_COL] = str(int(row[PRIORITY_COL]) + 4)
+        elif row[COMPLETED_COL] == 'TRUE' and row[DAILY_COL] == 'TRUE':
+            row[PRIORITY_COL] = str(9)
+            row[COMPLETED_COL] = "FALSE"
+        else:
+            row[PRIORITY_COL] = str(int(row[PRIORITY_COL]) + 1)
+    write_rows_to_file(rows)
+
+
+def add_task():
+    name = input("What is the name of your task?")
+    priority = input("In how many days is this task due? Enter 'Daily' if this is a daily task.")
+    try:
+        priority = str(MAX_PRIORITY + 1 - int(priority))
+        daily = 'FALSE'
+    except ValueError:
+        if 'daily' in priority.lower():
+            daily = 'TRUE'
+            priority = 9
+        else:
+            print("The value you entered was not a valid option. Please try again")
+            return
+    row = [[name, priority, daily, 'FALSE']]
+    rows = open_file_and_get_rows()
+    rows = rows + row
+    write_rows_to_file(rows)
+    print("Success!")
 
 
 if __name__ == '__main__':
     main(sys.argv[1:])
-
